212129260b976b147b04aae1054e5a223c74f59b
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-03-21 20:43:33 -0400

    "Set up the startup configuration"

---
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -0,0 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace ElevenNote.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options) { }
+}
