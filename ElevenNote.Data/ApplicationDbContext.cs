9898dffac442b462aaa658dd31c477cc39ed34dc
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-03-27 17:57:12 -0400

    "Established the user table"

--- ElevenNote.Data/ApplicationDbContext.cs
+++ ElevenNote.Data/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using ElevenNote.Data.Entities;
 
 namespace ElevenNote.Data;
 
@@ -6,4 +7,6 @@ public class ApplicationDbContext : DbContext
 {
     public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
         : base(options) { }
+
+    public DbSet<UserEntity> Users { get; set; } = null!;   
 }
