2cc051f5efc9bd6181de144555f037004c0fed96
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-03-27 17:54:21 -0400

    "Added Data Annotations User Entity"

--- ElevenNote.Data/Entities/UserEntity.cs
+++ ElevenNote.Data/Entities/UserEntity.cs
@@ -2,17 +2,24 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace ElevenNote.Data.Entities
 {
     public class UserEntity
     {
+        [Key]
         public int Id { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; } = string.Empty;
+        [Required]
         public string Username { get; set; } = string.Empty;
+        [Required]
         public string Password { get; set; } = string.Empty;
         public string? FirstName { get; set; }
         public string? LastName { get; set; }
+        [Required]
         public DateTime DateCreated { get; set; }
     }
 }
