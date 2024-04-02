327ea879f6bbe9ad860f72cf55c6aef28a912687
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-04-02 09:23:54 -0400

    "Added GetUserById Controller Action"

--- ElevenNote.WebAPI/Controllers/UserController.cs
+++ ElevenNote.WebAPI/Controllers/UserController.cs
@@ -27,5 +27,17 @@ namespace ElevenNote.WebAPI.Controllers
             }
             return BadRequest("User could not be registered.");
         }
+        [HttpGet("{userId:int}")]
+        public async Task<IActionResult> GetById([FromRoute] int userId)
+        {
+            var userDetail = await _userService.GetUserByIdAsync(userId);
+
+            if (userDetail is null)
+            {
+                return NotFound();
+            }
+
+                return Ok(userDetail);
+        }
     }
 }
