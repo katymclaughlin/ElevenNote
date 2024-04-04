1e3a75bc89dd7fa9cf57a3314d85b1843f7235da
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-04-04 19:15:14 -0400

    "Note Entity"

---
+++ ElevenNote.Data/Entities/NoteEntity.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ElevenNote.Data.Entities
+{
+    public class NoteEntity
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int OwnerId { get; set; }
+        [Required]
+        [MinLength(2), MaxLength(100)]
+        public string Title { get; set; } = string.Empty;
+        [Required]
+        [MaxLength(8000)]
+        public string Content { get; set; } = string.Empty;
+        [Required]
+        public DateTimeOffset CreatedUtc { get; set; }
+        public DateTimeOffset? ModifiedUtc { get; set; }
+    }
+}
