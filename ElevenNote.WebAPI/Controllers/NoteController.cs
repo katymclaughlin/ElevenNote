a48687be48178a51f3d9e7597e0c57ec5d25174b
Katy McLaughlin <mclaughlinkaty6@gmail.com>
2024-04-04 20:29:42 -0400

    "Completed Note Controller"

---
+++ ElevenNote.WebAPI/Controllers/NoteController.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using ElevenNote.Services.Note;
+using Microsoft.AspNetCore.Authorization;
+
+namespace ElevenNote.WebAPI.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class NoteController : ControllerBase
+    {
+        private readonly INoteService _noteService;
+        public NoteController(INoteService noteService)
+        {
+            _noteService = noteService;
+        }
+    }
+}
